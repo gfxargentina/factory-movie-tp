d8988a947abaa39daf5dff3b6a31cd95c1394e8e
Luis Chilo <gfxargentina@gmail.com>
2022-11-01 12:48:33 -0300

    add: sequelize db conection

---
+++ src/database/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config/db-config');
+dotenv.config();
+
 const app = express();
 
-dotenv.config();
+//conexion postgresql
+dbConnection();
+
+//middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//enpoints
+//app.use('/auth', require('./routes/auth.routes'));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server Listening in', process.env.PORT);
 });
+
+module.exports = app;
