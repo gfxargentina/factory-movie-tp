1fb6e29929ae5363eb746f5ff953732f73b7e46f
Luis Chilo <gfxargentina@gmail.com>
2022-11-09 00:57:09 -0300

    add: unit tests and coverage

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,4 +1,13 @@
+//During the test the env variable is set to test
+//process.env.NODE_ENV = 'test';
+
 const { expect } = require('chai');
+require('chai').should();
+
+let chai = require('chai'),
+  chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 const { response } = require('express');
 const request = require('supertest');
@@ -18,123 +27,117 @@ const favoriteModel = require('../database/models').Favorite;
 //   db.sequelize.truncate({ cascade: true });
 // });
 
-describe('GET /movies', () => {
-  it('Should return status 200', async function () {
-    //request(app).get('/movie').expect(201).end(done());
-    let data = {
-      firstName: 'Charly',
-      lastName: 'Garcia',
-      email: 'charly@gmail.com',
-      password: '12345678',
-      address: 'BA',
-    };
-
-    await request(app).post('/auth/register').send(data); //depends on above [Signup].
-
-    let dataB = {
-      email: 'charly@gmail.com',
-      password: '12345678',
-    };
-
-    let res = await request(app).post('/auth/login').send(dataB); //depends on above [Login].
-
-    let token = res.body.token;
-
-    res = await request(app)
-      .get('/movie')
-      .set('Authorization', 'bearer ' + token);
-
-    expect(res.status).to.equal(200);
-  });
-
-  it('Should return json', function (done) {
+describe('Post /Movie ', () => {
+  let token;
+  before(function (done) {
     request(app)
-      .get('/movie')
-      .set('Accept', 'application/json')
-      .expect(200, done());
+      .post('/auth/login')
+      .send({
+        email: 'charly@gmail.com',
+        password: '12345678',
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        token = res.body.token;
 
-    //.end(done());
+        done();
+      });
+  });
+  it('Should Add a New Movie', async () => {
+    const newMovie = {
+      title: 'Pelicula Test',
+    };
+    const res = await chai
+      .request(app)
+      .post('/movie/')
+      .set('authorization', 'Bearer ' + token)
+      .send(newMovie);
+
+    res.should.have.status(201);
+    res.body.should.be.a('object');
+    res.body.should.have.property('msg');
   });
 
-  // it('Should return movies', (done) => {
-  //   request(app)
-  //     .get('/movie')
-  //     .expect(200)
-  //     .then((response) => {
-  //       assert.isNotEmpty(response._body);
-  //       assert.isArray(response._body);
-  //       respons._body.forEach((movie) =>
-  //         assert.containsAllKeys(movie, [
-  //           'title',
-  //           'description',
-  //           'director',
-  //           'producer',
-  //           'release_date',
-  //           'running_time',
-  //           'rt_score',
-  //         ])
-  //       );
-  //     })
-  //     .then(() => done(), done()); // soluciona el problema de  Error: Timeout of 2000ms exceeded.
-  // });
+  it('Should show one movie details by id ', async () => {
+    const res = await chai
+      .request(app)
+      .get('/movie/12')
+      .set('authorization', 'Bearer ' + token);
+    console.log(res.body);
+
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('movie');
+    res.body.movie.should.have.property('id');
+    res.body.movie.should.have.property('code');
+    res.body.movie.should.have.property('title');
+    res.body.movie.should.have.property('stock');
+    res.body.movie.should.have.property('rentals');
+    res.body.movie.should.have.property('UserId');
+    res.body.movie.should.have.property('FavoriteMovieId');
+  });
 });
 
-// describe('GET /movies/:id', () => {
-//   let token;
-//   before(function (done) {
-//     request(app)
-//       .post('/auth/login')
-//       .send({
-//         email: 'charly@gmail.com',
-//         password: '12345678',
-//       })
-//       .expect(200)
-//       .expect('Content-Type', /json/)
-//       .end(function (err, res) {
-//         token = res.body.token;
-//         console.log(token);
-//         done();
-//       });
-//   });
+describe('GET All /movies', () => {
+  it('Should return status 200 and be a json', async () => {
+    const res = await chai.request(app).get('/movie');
 
-//   it('Get Movie Details By ID', (done) => {
-//     request(app)
-//       .get('/movies/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
-//       .expect(200)
-//       .set('authorization', 'Bearer ' + token)
-//       .then((response) => {
-//         assert.isNotEmpty(response._body); //no esta vacio
-//         assert.isNotArray(response._body);
-//         assert.containsAllKeys(response._body, [
-//           'title',
-//           'description',
-//           'director',
-//           'producer',
-//           'release_date',
-//           'running_time',
-//           'rt_score',
-//         ]);
-//       })
-//       .then(() => done(), done);
-//   });
-// });
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a('object');
+    res.body.should.have.property('movies');
+    res.body.movies.should.be.a('array');
+    res.body.movies[0].should.have.property('title');
+    res.body.movies[0].should.have.property('release_date');
+  });
+});
+
+describe('GET /movies Details', () => {
+  let token;
+  before(function (done) {
+    request(app)
+      .post('/auth/login')
+      .send({
+        email: 'charly@gmail.com',
+        password: '12345678',
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        token = res.body.token;
+        done();
+      });
+  });
+
+  it('Get Movies Details ', async () => {
+    const res = await chai
+      .request(app)
+      .get('/movie/details')
+      .set('authorization', 'Bearer ' + token);
+
+    res.should.have.status(200);
+    res.body.should.be.a('array');
+    res.body[0].should.have.property('id');
+    res.body[0].should.have.property('title');
+    res.body[0].should.have.property('description');
+    res.body[0].should.have.property('director');
+    res.body[0].should.have.property('producer');
+    res.body[0].should.have.property('release_date');
+    res.body[0].should.have.property('running_time');
+    res.body[0].should.have.property('rt_score');
+  });
+});
 
 describe('POST register and login', () => {
   const userExample = {
-    email: 'test@gmail.com',
+    firstName: 'Ethereal 04',
+    lastName: 'Email',
+    email: 'example4@email.com',
     password: '12345678',
-    firstName: 'mocha',
-    lastName: 'test',
     address: 'Testing 666',
+    verified: true,
   };
-
-  it('should return 201', (done) => {
-    request(app)
-      .post('/auth/register')
-      .send(userExample)
-      .expect(201)
-      .then(() => done(), done());
-  });
 
   it('should user register', (done) => {
     request(app)
@@ -183,18 +186,8 @@ describe('POST register and login', () => {
   });
 });
 
-describe.only('POST /login', () => {
+describe('POST /login', () => {
   it('should login user and return auth token', async function () {
-    let data = {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'hi@johndoe.com',
-      password: '12345678',
-      address: 'Testing 666',
-    };
-
-    await request(app).post('/auth/register').send(data); //depends on above [signup].
-
     data = {
       email: 'hi@johndoe.com',
       password: '12345678', // correct pass
@@ -207,19 +200,9 @@ describe.only('POST /login', () => {
   });
 
   it('Should return error if wrong pass / email provided', async function () {
-    let data = {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'hi@johndoe.com',
-      password: '12345678',
-      address: 'Testing 666',
-    };
-
-    await request(app).post('/auth/register').send(data); //depends on above [Signup].
-
     data = {
       email: 'hi@johndoe.com',
-      password: '2345678', //wrong pass
+      password: '2345678',
     };
 
     let res = await request(app).post('/auth/login').send(data);
@@ -242,55 +225,21 @@ describe('POST /favourite/:code', () => {
       .expect('Content-Type', /json/)
       .end(function (err, res) {
         token = res.body.token;
-        console.log(token);
+
         done();
       });
   });
 
-  it('Should return 201 and set movie as favourite for logged user with review', async function (done) {
-    let data = {
-      email: 'hi@johndoe.com',
-      password: '12345678',
-    };
+  it('Should return 201 and set movie as favourite for logged user with review', async () => {
+    const res = await chai
+      .request(app)
+      .post('/movie/favorite/5fdfb320-2a02-49a7-94ff-5ca418cae602')
+      .set('authorization', 'Bearer ' + token);
 
-    let user = await request(app).post('/auth/login').send(data); //depends on above [signup].
-
-    data = {
-      email: 'hi@johndoe.com',
-      password: '12345678', // correct pass
-    };
-
-    let res = await request(app).post('/auth/login').send(data);
-
-    expect(res.status).to.equal(200);
-    expect(res.body).to.have.property('token');
-    // request(app)
-    //   .post('/movie/favorite/45db04e4-304a-4933-9823-33f389e8d74d')
-    //   .set('authorization', 'Bearer ' + token)
-    //   .expect(201)
-    //   .then(async (res) => {
-    //     assert.isNotEmpty(res.body); //no esta vacio
-
-    //     const favoriteDB = await favoriteModel.findOne({
-    //       where: { code: '45db04e4-304a-4933-9823-33f389e8d74d' },
-    //     });
-    //     assert.exists(favoriteDB);
-    //   })
-    //   .then(() => done(), done());
+    res.should.have.status(201);
+    res.body.should.be.a('object');
+    res.body.should.have.property('msg');
   });
-
-  // it('Should return 201 and set movie as favourite for logged user without review', (done) => {
-  //   // TO-DO
-  //   // Check status
-  //   // Check si se registro el cambio en la DB
-  //   // Check si el registro en la DB es correcto
-  // });
-  // it('Should not allow to favourite the same movie twice', (done) => {
-  //   //TO-DO, llamar al endpoint con la misma peli 2 veces
-  //   // Check error status
-  //   // Check error message
-  //   // Check db que no se haya persistido un registro
-  // });
 });
 
 describe('GET /favourites', () => {
@@ -309,24 +258,58 @@ describe('GET /favourites', () => {
         done();
       });
   });
-  it('Should return 200 status and logged user favourite list', function (done) {
-    // TO-DO
-    // checkear que sea un array
-    // checkear que tenga la cantidad correcta de elementos
-    // checkear las clave de cada elemento
-    // checkear que los elementos sean/sea el/los correctos
-
-    request(app)
+  it('Should return 200 status and logged user favourite list', async () => {
+    const res = await chai
+      .request(app)
       .get('/movie/favorite')
-      .set('authorization', 'Bearer ' + token)
-      .expect(201)
-      .then(() => done(), done());
+      .set('authorization', 'Bearer ' + token);
+
+    res.should.have.status(200);
+    res.body.should.be.a('array');
+    res.body[0].should.have.property('id');
+    res.body[0].should.have.property('MovieCode');
+    res.body[0].should.have.property('UserId');
   });
-  //   it('Should forbid access to non logged user', (done) => {
-  //     //TO-DO
-  //     //Chequear status
-  //     //Chequear mensaje de error
-  //   });
+});
+
+describe('Post /Rental ', () => {
+  let token;
+  before(function (done) {
+    request(app)
+      .post('/auth/login')
+      .send({
+        email: 'charly@gmail.com',
+        password: '12345678',
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        token = res.body.token;
+
+        done();
+      });
+  });
+  it('Should Rent a Movie', async () => {
+    const res = await chai
+      .request(app)
+      .post('/rental/ff24da26-a969-4f0e-ba1e-a122ead6c6e3')
+      .set('authorization', 'Bearer ' + token);
+
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('newRental');
+  });
+
+  it('Should List all Movies Rental from user', async () => {
+    const res = await chai
+      .request(app)
+      .get('/user/rentals')
+      .set('authorization', 'Bearer ' + token);
+
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('movies');
+  });
 });
 
 // describe('POST /rent/:code', () => {
@@ -385,6 +368,34 @@ describe('GET /favourites', () => {
 //     //TO-DO
 //   });
 //});
+
+describe('Negative Tests', () => {
+  let token;
+  before(function (done) {
+    request(app)
+      .post('/auth/login')
+      .send({
+        email: 'charly@gmail.com',
+        password: '12345678',
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        token = res.body.token;
+
+        done();
+      });
+  });
+
+  it('Should not return all favorite movies, 401 Authentication Failed', async () => {
+    const res = await chai
+      .request(app)
+      .get('/movie/favorite')
+      .set('authorization', 'Bearer ');
+
+    res.should.have.status(401);
+  });
+});
 
 describe('Not Found handling', () => {
   it('Should return status 404', (done) => {
