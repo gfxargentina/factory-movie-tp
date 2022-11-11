4dc41366dac2c510ee31461fda74f2685cd09ab3
Luis Chilo <gfxargentina@gmail.com>
2022-11-11 00:58:11 -0300

    add: server home html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
 const { dbConnection } = require('./database/config/db-config');
 const { notFound } = require('./middlewares/handleError');
+const swaggerUi = require('swagger-ui-express');
+const { swaggerSpec } = require('./swagger');
 dotenv.config();
 
 const app = express();
@@ -15,12 +18,16 @@ dbConnection();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //enpoints
 app.use('/auth', require('./routes/auth.routes'));
 app.use('/user', require('./routes/user.routes'));
 app.use('/rental', require('./routes/rental.routes'));
 app.use('/movie', require('./routes/movie.routes'));
+app.use('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './views/home.html'));
+});
 
 app.use(notFound);
 
